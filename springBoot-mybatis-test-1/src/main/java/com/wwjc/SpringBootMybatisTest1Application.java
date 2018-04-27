6699a39cdb676c77a137ef4426fec81a30155fed
wenxuefeng <18200329369@163.com>
2018-04-27 15:06:14 +0800

    Signed-off-by: wenxuefeng <18200329369@163.com>

---
+++ springBoot-mybatis-test-1/src/main/java/com/wwjc/SpringBootMybatisTest1Application.java
@@ -0,0 +1,12 @@
+package com.wwjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisTest1Application.class, args);
+	}
+}
